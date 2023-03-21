57b06340f579f73201983e32f027aa77ca13eec8
Ömer Faruk Tokluoğlu <88704993+Bersarks@users.noreply.github.com>
2023-03-21 14:50:29 +0300

    if-else ve ternary dersi eklendi.

---
+++ C#101/if-else yapısı ve ternary/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int	time = DateTime.Now.Hour;
+
+if (time >= 6 && time <= 11)
+	Console.WriteLine("Good morning!");
+else if (time <= 18)
+	Console.WriteLine("Have a nice day!");
+else
+	Console.WriteLine("Good Night");
+
+string result = time <= 18 ? "Have a nice day!" : "Good night";
+Console.WriteLine(result);
+
+result = time >= 6 && time <= 11 ? "Good morning!" : time <= 18 ? "Have a nice day!" : "Good night";
+Console.WriteLine(result);
