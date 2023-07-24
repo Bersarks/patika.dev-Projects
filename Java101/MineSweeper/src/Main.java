e9c22ef3a6d87503aa8a15350697248ecf6354ce
Ömer Faruk Tokluoğlu <88704993+Bersarks@users.noreply.github.com>
2023-07-24 11:31:33 +0300

    Add hw and practices

---
+++ Java101/MineSweeper/src/Main.java
@@ -0,0 +1,15 @@
+import model.*;
+
+public class Main {
+	public static void main(String[] args) {
+		MineSweeper mineSweeper = new MineSweeper();
+		String[][] panel = mineSweeper.PanelOlustur();
+		String[][] bombapanel = new String[mineSweeper.satir][mineSweeper.sutun];
+		bombapanel = mineSweeper.Olustur(bombapanel);
+		mineSweeper.BombaPanel(bombapanel);//bombapanele bombaları yerleştiriyor
+		while (mineSweeper.kontrol) {
+			System.out.println("======================");
+			mineSweeper.OyunaBasla(panel, bombapanel);
+		}
+	}
+}
